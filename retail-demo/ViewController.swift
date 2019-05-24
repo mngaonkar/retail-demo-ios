dddcbe0a91b4499e29e6ac9fae642359aeb1bf79
Mahadev Gaonkar <mahadev.gaonkar@gmail.com>
2019-05-25 00:39:01 +0530

    microphone icon added

--- retail-demo/ViewController.swift
+++ retail-demo/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import Speech
 import Meridian
 
 class ViewController: UIViewController, MRMapViewDelegate, MRLocationManagerDelegate {
@@ -15,6 +16,8 @@ class ViewController: UIViewController, MRMapViewDelegate, MRLocationManagerDele
     var mapView: MRMapView!
     var locationManager: MRLocationManager!
     let synthesizer = AVSpeechSynthesizer()
+    let audioEngine = AVAudioEngine()
+    let recognizer = SFSpeechRecognizer()
     
     let MAP_ID = "5764017373052928"
     let APP_ID = "5737079267393536"
@@ -22,6 +25,8 @@ class ViewController: UIViewController, MRMapViewDelegate, MRLocationManagerDele
     func mapPickerDidPick(_ map: MRMap) {
         
     }
+    
+    @IBOutlet weak var voiceButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,14 +36,22 @@ class ViewController: UIViewController, MRMapViewDelegate, MRLocationManagerDele
         locationManager.delegate = self
         
         mapView = MRMapView(frame: self.view.bounds)
-        // mapView.delegate = self
+        mapView.delegate = self
         
         mapView.mapKey = MREditorKey(forMap: MAP_ID, app: APP_ID)
-        self.view.addSubview(mapView)
         
-        speak(text: "Welcome to meridian demo, have a great day ahead")
+        // self.view.addSubview(mapView)
+        voiceButton.layer.cornerRadius = voiceButton.frame.height/2
+        voiceButton.layer.shadowOpacity = 0.75
+        voiceButton.layer.shadowRadius = 5
+            
+        textToSpeech(text: "Welcome to meridian demo, have a great day ahead")
     }
 
+    @IBAction func voiceButtonClicked(_ sender: Any) {
+        
+    }
+    
     func mapView(_ mapView: MRMapView, rendererFor overlay: MRPathOverlay) -> MRPathRenderer? {
         let renderer = MRPathRenderer(overlay: overlay)
         
@@ -50,7 +63,7 @@ class ViewController: UIViewController, MRMapViewDelegate, MRLocationManagerDele
         
     }
     
-    func speak(text: String) {
+    func textToSpeech(text: String) {
         let utterance = AVSpeechUtterance(string: text)
         synthesizer.speak(utterance)
     }
